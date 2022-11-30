8fd6ab9cd423fef1ffdc4ad8070bce5f70e73d9b
Ricardo Pacheco <ricardogpacheco999@gmail.com>
2022-11-30 16:31:46 -0300

    .

--- main.c
+++ main.c
@@ -2,8 +2,26 @@
 #include <stdlib.h>
 #include "products.h"
 
+/**
+ * @brief Print the menu with the user options
+ * 
+ */
+void printMenu() {
+
+  print()
+};
+
+/**
+ * @brief Print the system information
+ * 
+ * @return int 
+ */
+void printSystemInfo() {
+
+};
+
 int main(void) {
-    Products* products = readProductsFile("products.txt");
-    freeProducts(products);
-    return 0;
+  Products* products = readProductsFile("products.txt");
+  freeProducts(products);
+  return 0;
 }
